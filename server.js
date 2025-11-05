a52602aff95a52e184dfd6a65e7c035d29d54eb7
Paul Jenkins <paul@turnpiece.com>
2025-11-05 17:47:36 +0000

    Updated CORS frame-src directive removing the regex part

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     "img-src 'self' data: https:",
     "font-src 'self'",
     "connect-src 'self' https://stagingapi.temphist.com https://devapi.temphist.com https://api.temphist.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://nominatim.openstreetmap.org https://ipapi.co https://*.firebaseapp.com https://*.googleapis.com",
-    "frame-src 'self' https://temphist-2c787.firebaseapp.com https://*.firebaseapp.com",
+    "frame-src 'self' https://temphist-2c787.firebaseapp.com https://*.firebaseapp.com https://*.googleapis.com",
     "object-src 'self' data:",
     "base-uri 'self'",
     "form-action 'self'",
